0fcce5bbe9189294013f820d64ab326848415aca
WycliffeAlphus <wycliffealphus7@gmail.com>
2024-07-30 14:28:18 +0300

    feat:add proof of authority

--- pests.go
+++ pests.go
@@ -8,26 +8,46 @@ import (
 	"time"
 )
 
+// Authority represents an authorized entity that can add blocks
+type Authority struct {
+	Name string
+}
+
+type Data struct {
+	Location string
+	Crop     string
+	Price    float64
+}
+
+// AuthorizedAuthorities is a list of authorized nodes (authorities)
+var AuthorizedAuthorities = map[string]Authority{
+	"Authority1": {Name: "Authority1"},
+	"Authority2": {Name: "Authority2"},
+}
+
+// PestReport represents a blaock in the blockchain
 type PestReport struct {
-	Data         interface{}
+	MarketInfo   Data
 	TimeStamp    time.Time
 	PreviousHash string
 	Hash         string
+	Authority    string // Track which authority created the block
 }
 
+// ReportsRecord represents the entire blockchain
 type ReportsRecord struct {
 	Chain []PestReport
 }
 
 // CreateHash calculates and sets the hash for the block
 func (block *PestReport) CreateHash() {
-	newDataString, err := json.Marshal(block.Data)
+	newDataString, err := json.Marshal(block.MarketInfo)
 	if err != nil {
 		fmt.Println("error encoding data:", err)
 		return
 	}
 
-	blockString := string(newDataString) + block.PreviousHash + block.TimeStamp.Format(time.RFC3339)
+	blockString := string(newDataString) + block.PreviousHash + block.TimeStamp.Format(time.RFC3339) + block.Authority
 
 	blockHash := sha256.Sum256([]byte(blockString))
 
@@ -37,8 +57,9 @@ func (block *PestReport) CreateHash() {
 // CreateGenesis initializes the genesis block and calculates its hash
 func CreateGenesis() ReportsRecord {
 	genesis := PestReport{
-		Data:      "The first record",
-		TimeStamp: time.Now(),
+		MarketInfo: Data{},
+		TimeStamp:  time.Now(),
+		Authority:  "Authority1", // Initial authority
 	}
 	genesis.CreateHash()
 	return ReportsRecord{
@@ -47,13 +68,17 @@ func CreateGenesis() ReportsRecord {
 }
 
 // CreateBlock creates a new block and returns a new ReportsRecord with the new block appended
-func (b ReportsRecord) CreateBlock() ReportsRecord {
-	imgByte := []byte{0, 1} // Example data
+func (b ReportsRecord) CreateBlock(authorityName string, data Data) ReportsRecord {
+	if _, authorized := AuthorizedAuthorities[authorityName]; !authorized {
+		fmt.Println("Error: Unauthorized authority")
+		return b
+	}
 
 	block := PestReport{
-		Data:         imgByte,
+		MarketInfo:   Data{},
 		PreviousHash: b.Chain[len(b.Chain)-1].Hash,
 		TimeStamp:    time.Now(),
+		Authority:    authorityName,
 	}
 
 	block.CreateHash()
@@ -66,15 +91,16 @@ func (b ReportsRecord) CreateBlock() ReportsRecord {
 func (b ReportsRecord) String() string {
 	var result string
 	for _, block := range b.Chain {
-		dataStr, err := json.Marshal(block.Data)
+		dataStr, err := json.Marshal(block.MarketInfo)
 		if err != nil {
 			dataStr = []byte("error marshalling data")
 		}
-		result += fmt.Sprintf("TimeStamp: %s\nPreviousHash: %s\nHash: %s\nData: %s\n\n",
+		result += fmt.Sprintf("TimeStamp: %s\nPreviousHash: %s\nHash: %s\nData: %s\nAuthority: %s\n\n",
 			block.TimeStamp.Format(time.RFC3339),
 			block.PreviousHash,
 			block.Hash,
-			string(dataStr))
+			string(dataStr),
+			block.Authority)
 	}
 	return result
 }
@@ -82,7 +108,13 @@ func (b ReportsRecord) String() string {
 func main() {
 	blockchain := CreateGenesis()
 
-	blockchain = blockchain.CreateBlock()
+	// Simulate block creation by an authorized authority
+	info1 := Data{Location: "Kisumu", Crop: "Rice", Price: 150}
+	blockchain = blockchain.CreateBlock("Authority1", info1)
+
+	// Attempt to create a block with an unauthorized authority
+	info2 := Data{Location: "Kisumu", Crop: "Rice", Price: 150}
+	blockchain = blockchain.CreateBlock("UnauthorizedAuthority", info2)
 
 	fmt.Println(blockchain.String())
 }
