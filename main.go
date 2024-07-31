5642c7b13ccee64aa0218d4ec16b09772b735092
somotto <stephenonyango4783@gmail.com>
2024-07-31 09:24:01 +0300

    feat:add main.go

--- main.go
+++ main.go
@@ -3,17 +3,17 @@ package main
 import (
 	"net/http"
 
-	"agri-mentor/server"
+	server "agri-mentor/agri"
 )
 
 func main() {
 	// Serve static files from the /static directory
-    http.Handle("/", http.FileServer(http.Dir("./templates")))
+	http.Handle("/", http.FileServer(http.Dir("./templates")))
 
 	// http.HandleFunc("/", server.RenderTemplate) // Serve the HTML file
-	
+
 	http.HandleFunc("/query", server.HandleSendMessage) // Handle the input submission
 
-	println("Server started at port: 8080")
-	http.ListenAndServe(":8080", nil) // Start the server
+	println("Server started at port: 8081")
+	http.ListenAndServe(":8081", nil) // Start the server
 }
